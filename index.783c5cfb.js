2dcf728686f0e7818c3291b304783eb1a4b1327f
Yaroslav Samoilenko <yasik2255@live.com>
2023-12-08 16:18:29 +0200

    make build

---
+++ index.783c5cfb.js
@@ -0,0 +1,3 @@
+var e=document.getElementById("top-bar"),t=document.getElementById("page-shader");function n(){t.setAttribute("style","height: ".concat(e.offsetHeight,"px;"))}//# sourceMappingURL=index.783c5cfb.js.map
+window.onresize=n,n();
+//# sourceMappingURL=index.783c5cfb.js.map
